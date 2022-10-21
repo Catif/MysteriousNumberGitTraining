c438362d4c4051b6e3921a25ae7d4a06e1e0634a
Catif <bradley.barbier@outlook.fr>
2022-10-21 10:38:57 +0200

    Create Navbar Component
    create HTML SCSS and JS

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
+import Navbar from './navbar.js'
+import UI from './ui.js'
 import Mysterious from './game.js'
 
 let App = {
     init(){
+        Navbar.createNavListener()
+        UI.init()
+        
         console.log(Mysterious.generateRandomNumber(100))
     }
 }
