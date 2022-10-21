e9e6e058472ef9fd38beaab0fd0bb2ac2218bd55
J-Juliiien <julien.joliot@outlook.fr>
2022-10-21 11:19:34 +0200

    Récupérer le nombre entré par l'utilisateur

--- js/game.js
+++ js/game.js
@@ -2,6 +2,20 @@ let generateRandomNumber = function(max){
     return Math.floor(Math.random() * max)
 }
 
+const getNumber = document.querySelector("#number")
+const btnResult = document.querySelector("#btnResult")
+const displayNumber = document.querySelector("#displayNumber")
+
+function userEntry(){
+    btnResult.addEventListener('click', function() {
+        return getNumber.value
+    })
+}
+
+let number = userEntry()
+
+
+
 export default {
     generateRandomNumber
 }
